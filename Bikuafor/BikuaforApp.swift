5ca8b49024e0f6d609211727fd2199dc5facffa5
Hatice Taşdemir <tdemirhtc@gmail.com>
2024-11-27 13:17:12 +0300

    Initial Commit

---
+++ Bikuafor/BikuaforApp.swift
@@ -0,0 +1,17 @@
+//
+//  BikuaforApp.swift
+//  Bikuafor
+//
+//  Created by Hatice Taşdemir on 27.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct BikuaforApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
